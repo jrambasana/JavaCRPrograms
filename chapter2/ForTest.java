4a275ca2c677edef440248e728e39adc14388b2b
Jatin Ambasana <jatinambasana@gmail.com>
2023-12-26 12:29:51 +0530

    Update ForTest.java

--- chapter2/ForTest.java
+++ chapter2/ForTest.java
@@ -2,10 +2,10 @@ package chapter2;
 
 public class ForTest {
     public static void main(String args[]){
-
-        int x;
-        for(x=0;x<10;x=x+1){
-            System.out.println("This is x :"+x);
+    	
+        for( int x = 0 ; x < 10 ; x = x + 1 ){
+            System.out.println("This is x :" + x);
         }
+        
     }
 }
