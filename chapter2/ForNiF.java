9acee5c4fb2b1fe9c71cb729987efca15728581d
Jatin Ambasana <jatinambasana@gmail.com>
2023-12-26 15:58:58 +0530

    Create ForNiF.java

---
+++ chapter2/ForNiF.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class ForNiF {
+    public static void main(String args[]){
+    	
+        for( int x = 1 ; x <= 100 ; x = x + 1 )
+        	if( x%2 != 0 )
+        		System.out.println("Odd Number: " + x);
+        for( int x = 1 ; x <= 100 ; x = x + 1 )
+        	if( x%2 == 0 )
+            	System.out.println("Even Number: " + x);
+        	        
+    }
+}
