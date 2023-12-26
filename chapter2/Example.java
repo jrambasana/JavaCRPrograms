1b06daac778d41ac8b9cd976931dba4621243269
Jatin Ambasana <jatinambasana@gmail.com>
2023-12-26 11:49:30 +0530

    Update Example.java

--- chapter2/Example.java
+++ chapter2/Example.java
@@ -2,6 +2,7 @@
 /*This is a simple Java Program */
 public class Example {
     public static void main(String args[]) {
-        System.out.println("This is a simple Java program");
+        
+    	System.out.println("This is a simple Java program");
     }
 }
