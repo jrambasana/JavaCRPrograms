27a2a40178c3c2653483259c8f664ce90ba20191
Jatin Ambasana <jatinambasana@gmail.com>
2023-12-26 11:01:58 +0530

    Programs added

---
+++ chapter2/Example.java
@@ -0,0 +1,9 @@
+ package chapter2;
+/*This is a simple Java Program */
+public class Example {
+    public static void main(String args[]) {
+
+        System.out.println("This is a simple Java program");
+    }
+}
+
