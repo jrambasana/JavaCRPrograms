19ed66bea6e76d80e0620558e2ad85ddf5ea4889
Jatin Ambasana <jatinambasana@gmail.com>
2023-12-26 11:50:31 +0530

    Update Example.java

--- chapter2/Example.java
+++ chapter2/Example.java
@@ -1,8 +1,13 @@
- package chapter2;
+package chapter2;
+
 /*This is a simple Java Program */
+
 public class Example {
+	
     public static void main(String args[]) {
-        
+    	
     	System.out.println("This is a simple Java program");
+    	
     }
+    
 }
